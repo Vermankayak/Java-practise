98803d5eb44b08c5f3d89616dba33bdf7309c4b6
Nihar Kuchroo <niharku@gmail.com>
2024-04-15 12:45:01 +0530

    Inner classes - 1

--- Practise/src/Practise.java
+++ Practise/src/Practise.java
@@ -1,7 +1,21 @@
-
-
-public class Practise {
+class Computer{
 	
+	public void config() {
+		System.out.println("Configured your old computer");
+	}
 	
+}
+public class Practise{
 
+	public static void main(String [] args) {
+		
+		Computer comp  = new Computer() {
+			public void config() {
+				System.out.println("Configured your computer");
+			}
+		};
+		
+		comp.config();
+		
+	}
 }
