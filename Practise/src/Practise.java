b2eb5b3fbef3e36488b2afe7a27f7a4626361e28
Nihar Kuchroo <niharku@gmail.com>
2024-04-20 17:15:53 +0530

    Learning about Threads in Java

--- Practise/src/Practise.java
+++ Practise/src/Practise.java
@@ -1,34 +1,62 @@
-import java.util.ArrayList;
+class Child1 implements Runnable{
 
-class Student{
-	void disp(){
-		
-	}
+    @Override
+    public void run(){
+        try{
+        	System.out.println("Child1 thread entering into sleep state");
+        	Thread.sleep(3000);
+        	System.out.println("Child1 thread entered into sleep state");
+        }catch(Exception e){
+        	System.out.println("Child1 thread was interrupted");
+        }
+       
+        for(int i = 0; i< 5; i++){
+            System.out.println("Inside Child1 thread");
+        }
+    }
 }
 
-//class Child extends Parent{
-//	void disp() throws ArithmeticException {
-//		
-//	}
-//}
+class Child implements Runnable{
+
+    @Override
+    public void run(){
+        try{
+        	Child1 c = new Child1();
+            
+            Thread t = new Thread(c);
+            
+        	System.out.println("Child thread entering into sleep state");
+        	t.start();
+        	t.join();
+        	System.out.println("Child thread entered into sleep state");
+        }catch(Exception e){
+        	System.out.println("Child thread was interrupted");
+        }
+       
+        for(int i = 0; i< 5; i++){
+            System.out.println("Inside Child thread");
+        }
+    }
+}
+
+public class Practise{
 
 
-public class Practise  {
-	static int [] a;
-	
-	public static void main(String... args) {
-		
-		ArrayList al = new ArrayList();
-		
-		al.add(new Student());
-		
-		Object o = al.get(0);
-		
-		System.out.println(o instanceof Student);
-		
-	
-		
-		
-	}
-	
- }
+    public static void main(String[] args) throws InterruptedException {
+        
+        Child c = new Child();
+        
+        Thread t = new Thread(c);
+       
+
+        t.start();
+        t.interrupt();
+
+
+        for(int i = 0; i< 5; i++){
+            System.out.println("Inside main thread");
+        }
+        System.out.println("End of main thread");
+        
+    }
+}
